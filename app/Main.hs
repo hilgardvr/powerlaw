ca020c743be61f73fb173e386b84feaaaa1d8855
hilgard <hilgardvr@gmail.com>
2024-05-27 18:50:45 +0200

    Cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ import CoinGeckoClient (Price, getPrice)
 import Data.Time (getCurrentTime, UTCTime)
 import Pricing (daysFromGenesis, formula, annualised)
 import Data.List
+import Text.Printf (printf)
 
 yearDays :: Integer
 yearDays = 365
@@ -16,43 +17,31 @@ main = do
     env <- getLocalEnv
     p <- getPrice env
     now <- getCurrentTime
-    print $ "Current Price: " ++ show p
-    mapM_ (print . intercalate "  -  ")  
-        [ outputYearFromNowComparison 0 p now
-        , outputYearFromNowComparison 1 p now
-        , outputYearFromNowComparison 2 p now
-        , outputYearFromNowComparison 3 p now
-        , outputYearFromNowComparison 4 p now
-        , outputYearFromNowComparison 5 p now
-        , outputYearFromNowComparison 8 p now
-        , outputYearFromNowComparison 16 p now
-        ]
-    
+    let years = take 15 (0 : [ 2^i | i <- [0..] ]) 
+        disp = map (outputYearFromNowComparison p now)  years
+    print $ "---- Current Price: " ++ show p ++ " ----"
+    mapM_ (print . intercalate "  -  ")  disp
+        --[ outputYearFromNowComparison p now 0
+        --, outputYearFromNowComparison p now 1
+        --, outputYearFromNowComparison p now 2
+        --, outputYearFromNowComparison p now 3
+        --, outputYearFromNowComparison p now 4
+        --, outputYearFromNowComparison p now 5
+        --, outputYearFromNowComparison p now 8
+        --, outputYearFromNowComparison p now 16
+        --]
 
-getYearFromNowComparison :: Integer -> Price -> IO ()
-getYearFromNowComparison years exchangePrice = do
-    now <- getCurrentTime
-    let daysNow = daysFromGenesis now
-        formulaPrice = formula (daysNow + years * yearDays)
-    print $ show years ++ " years: $" ++  show formulaPrice
-    print $ "Current price $" ++  show exchangePrice
-    print $ "Implied total gain/(loss) in " ++ show years ++ " years %" ++ show (formulaPrice / exchangePrice * 100 - 100)
-    if years > 0
-    then print $ "Implied annualised gain/(loss) over " ++ show years ++ " years %" ++ show (annualised formulaPrice exchangePrice years)
-    else return ()
-    print $ "----"
-
-outputYearFromNowComparison :: Integer -> Price -> UTCTime -> [String]
-outputYearFromNowComparison years exchangePrice now =
+outputYearFromNowComparison :: Price -> UTCTime -> Integer -> [String] 
+outputYearFromNowComparison exchangePrice now years =
     let daysNow = daysFromGenesis now
         formulaPrice = formula (daysNow + years * yearDays)
         implied = 
             if years > 0
-            then "Implied annualised gain/(loss) over " ++ show years ++ " years %" ++ show (annualised formulaPrice exchangePrice years)
+            then "Implied annualised gain/(loss) over " ++ show years ++ " years %" ++ printf "%.2f" (annualised formulaPrice exchangePrice years)
             else ""
     in
-        [ ("Model price in " ++ show years ++ " years: $" ++  show formulaPrice)
-        , "Current price $" ++  show exchangePrice
-        , "Implied total gain/(loss) in " ++ show years ++ " years %" ++ show (formulaPrice / exchangePrice * 100 - 100)
+        [ ("Model price in " ++ show years ++ " years: $" ++  printf "%.2f" formulaPrice)
+        --, "Current price $" ++  printf "%.2f" exchangePrice
+        , "Implied total gain/(loss) in " ++ show years ++ " years %" ++ printf "%.2f" (formulaPrice / exchangePrice * 100 - 100)
         , implied
         ]
