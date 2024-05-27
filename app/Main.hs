dc60188bdc2d5c53a31c7a2b84ef0549541697fd
hilgard <hilgardvr@gmail.com>
2024-05-27 18:54:17 +0200

    Cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -17,19 +17,10 @@ main = do
     env <- getLocalEnv
     p <- getPrice env
     now <- getCurrentTime
-    let years = take 15 (0 : [ 2^i | i <- [0..] ]) 
+    let years = take 10 (0 : [ 2^i | i <- [0..] ]) 
         disp = map (outputYearFromNowComparison p now)  years
-    print $ "---- Current Price: " ++ show p ++ " ----"
+    print $ "---- Current Price: " ++ printf "%.2f" p ++ " ----"
     mapM_ (print . intercalate "  -  ")  disp
-        --[ outputYearFromNowComparison p now 0
-        --, outputYearFromNowComparison p now 1
-        --, outputYearFromNowComparison p now 2
-        --, outputYearFromNowComparison p now 3
-        --, outputYearFromNowComparison p now 4
-        --, outputYearFromNowComparison p now 5
-        --, outputYearFromNowComparison p now 8
-        --, outputYearFromNowComparison p now 16
-        --]
 
 outputYearFromNowComparison :: Price -> UTCTime -> Integer -> [String] 
 outputYearFromNowComparison exchangePrice now years =
@@ -41,7 +32,6 @@ outputYearFromNowComparison exchangePrice now years =
             else ""
     in
         [ ("Model price in " ++ show years ++ " years: $" ++  printf "%.2f" formulaPrice)
-        --, "Current price $" ++  printf "%.2f" exchangePrice
         , "Implied total gain/(loss) in " ++ show years ++ " years %" ++ printf "%.2f" (formulaPrice / exchangePrice * 100 - 100)
         , implied
         ]
